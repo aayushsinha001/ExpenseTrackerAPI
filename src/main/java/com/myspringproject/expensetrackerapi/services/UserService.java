fdd1badce584e58fc59f52acee1cda5e9f407e1d
aayush <aayush.1si14cs001@gmail.com>
2020-08-13 21:10:50 +0530

    Adding UserService Interface and Custom Exception class

---
+++ src/main/java/com/myspringproject/expensetrackerapi/services/UserService.java
@@ -0,0 +1,11 @@
+package com.myspringproject.expensetrackerapi.services;
+
+import com.myspringproject.expensetrackerapi.domain.User;
+import com.myspringproject.expensetrackerapi.exceptions.EtAuthException;
+
+public interface UserService {
+
+    User validateUser(String email,String password) throws EtAuthException;
+
+    User registerUser(String first, String lastName, String email, String password) throws EtAuthException;
+}
