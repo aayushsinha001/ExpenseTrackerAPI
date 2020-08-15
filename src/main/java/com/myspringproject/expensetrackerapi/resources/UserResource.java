125efdcefde9c6df116b0e10f60259330cfe19d4
aayush <aayush.1si14cs001@gmail.com>
2020-08-15 18:57:34 +0530

    Adding a json web token for authentication

--- src/main/java/com/myspringproject/expensetrackerapi/resources/UserResource.java
+++ src/main/java/com/myspringproject/expensetrackerapi/resources/UserResource.java
@@ -1,7 +1,10 @@
 package com.myspringproject.expensetrackerapi.resources;
 
+import com.myspringproject.expensetrackerapi.Constants;
 import com.myspringproject.expensetrackerapi.domain.User;
 import com.myspringproject.expensetrackerapi.services.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,9 +31,7 @@ public class UserResource {
         String email = (String) userMap.get("email");
         String password = (String) userMap.get("password");
         User user = userService.registerUser(firstName, lastName, email, password);
-        Map<String, String> map = new HashMap<>();
-        map.put("Message : ", "User successfully registered");
-        return new ResponseEntity<>(map, HttpStatus.OK);
+        return new ResponseEntity<>(generateJWTToken(user), HttpStatus.OK);
     }
 
     @PostMapping("/login")
@@ -37,8 +39,21 @@ public class UserResource {
         String email = (String) userMap.get("Email");
         String password = (String) userMap.get("Password");
         User user = userService.validateUser(email, password);
+        return new ResponseEntity<>(generateJWTToken(user), HttpStatus.OK);
+    }
+
+    private Map<String, String> generateJWTToken (User user) {
+        long timestamp = System.currentTimeMillis();
+        String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, Constants.API_SECRET_KEY)
+                .setIssuedAt(new Date(timestamp))
+                .setExpiration(new Date(timestamp + Constants.TOKEN_VALIDITY))
+                .claim("Email", user.getEmail())
+                .claim("UserId", user.getUserId())
+                .claim("FirstName", user.getFirstName())
+                .claim("LastName", user.getLastName())
+                .compact();
         Map<String, String> map = new HashMap<>();
-        map.put("User : ", "Logged in successfully");
-        return new ResponseEntity<>(map, HttpStatus.OK);
+        map.put("token", token);
+        return map;
     }
 }
